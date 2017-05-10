0b475ef57ec856a220f969c62cc949e738d9e6ad
rpalli <rohithpalli@gmail.com>
2017-05-10 17:01:40 -0400

    resaved sbatch file as .sh for better display in text editor

--- pyGArunner.sh
+++ pyGArunner.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 #SBATCH --partition=debug 
-
 #SBATCH --time=1:00:00 
 #SBATCH --mem-per-cpu=1g
 #SBATCH --cpus-per-task=24
@@ -8,7 +7,7 @@ export OMP_NUM_THREADS=24
 #SBATCH --output=slurm_out.txt -N 1 -J ruleMakerRun
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
-#SBATCH --array=1-10
+#SBATCH --array=1
 
 GADIR="/home/rpalli/rulemakerGA"
 OUTDIR="/public/rpalli/data/5-10-17"
@@ -19,8 +18,4 @@ cp $GADIR $OUTDIR
 cd $OUTDIR
 module load "intelpython/2.7.12"
 
-srun python 
-
-
-
-python GA.py $SLURM_ARRAY_TASK_ID > output.txt
+srun python networkConstructor.py
