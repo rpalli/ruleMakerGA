3e6461fd2d44b1e00eb81cb1d18138b9490d81ef
rpalli <rohithpalli@gmail.com>
2017-05-10 17:16:48 -0400

    eliminated quotes

--- pyGArunner.sh
+++ pyGArunner.sh
@@ -7,14 +7,14 @@ export OMP_NUM_THREADS=24
 #SBATCH --output=slurm_out.txt -N 1 -J ruleMakerRun
 #SBATCH --nodes=1
 echo "working"
-mkdir "/home/rpalli/data/5-10-17"
-GADIR="/home/rpalli/rulemakerGA"
-OUTDIR="/home/rpalli/data/5-10-17"
+mkdir /home/rpalli/data/5-10-17
+GADIR=/home/rpalli/rulemakerGA
+OUTDIR=/home/rpalli/data/5-10-17
 
 if [! -d $OUTDIR  ]
 	mkdir -p $OUTDIR
 cp $GADIR $OUTDIR
 cd $OUTDIR
-module load "intelpython/2.7.12"
+module load intelpython/2.7.12
 
 srun python networkConstructor.py
