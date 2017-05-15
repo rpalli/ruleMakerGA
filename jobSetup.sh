5403d0e531ea34fa8f184becd1aab569a55a8ec8
rpalli <rohithpalli@gmail.com>
2017-05-15 18:36:14 -0400

    permission fixes

--- jobSetup.sh
+++ jobSetup.sh
@@ -1,8 +1,8 @@
 
 GADIR=/home/rpalli/ruleMakerGA/
 OUTDIR=/home/rpalli/data/5-15-17/
-chmod -R 755 /home/rpalli/ruleMakerGA/pyNCsubmit.sh
 mkdir -p $OUTDIR
 cp -r $GADIR $OUTDIR
 cd $OUTDIR
+chmod -R 755 pyNCsubmit.sh
 sbatch pyNCsubmit.sh
--- updateRuleMakerGA.sh
+++ updateRuleMakerGA.sh
@@ -1,4 +1,4 @@
 
 rm -rf ruleMakerGA
 git clone https://github.com/rpalli/ruleMakerGA.git
-
+chmod -R 755 ruleMakerGA/jobSetup.sh
