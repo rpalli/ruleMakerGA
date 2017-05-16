808dfb7f9b462cabc63472a89d10ab169a2f29a4
rpalli <rohithpalli@gmail.com>
2017-05-15 20:44:43 -0400

    updated GA runner

--- GArunner.sh
+++ GArunner.sh
@@ -1,5 +1,5 @@
 chmod -R 755 pyGAsubmit.sh
 for graphfilename in *.gpickle; do
-	chmod -R 755 graphfilename;
-	sbatch pyGAsubmit.sh graphfilename
+	chmod -R 755 $graphfilename;
+	sbatch pyGAsubmit.sh $graphfilename
 done
