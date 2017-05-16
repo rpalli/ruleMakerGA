8e097a67f09b96dcee490ab11238747d59eec17f
rpalli <rohithpalli@gmail.com>
2017-05-15 20:01:22 -0400

    updated GAsubmit with stuff from info.circ...
    trying to get automatic insertion of array id

--- pyGAsubmit.sh
+++ pyGAsubmit.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #SBATCH --partition=standard 
-#SBATCH --time=1:00:00 
-#SBATCH --mem-per-cpu=1g
-#SBATCH --cpus-per-task=24
-#SBATCH --output=GA_out-%a.txt
-#SBATCH --nodes=1
-#SBATCH --array=1-10
+#SBATCH -a 1-10
+#SBATCH -J GArun
+#SBATCH -o GA_out-%a.txt
+#SBATCH --mem-per-cpu=100MB
+#SBATCH -t 10:00:00
+#SBATCH -n 1
+#SBATCH -c 24
+export OMP_NUM_THREADS=24
 
 module load intelpython/2.7.12
 srun python GA.py $1 $SLURM_ARRAY_TASK_ID
