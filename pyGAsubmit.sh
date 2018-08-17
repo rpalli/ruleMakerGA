44bf9e2de476983bd01743aa0d4ba4b05318d893
mgp13 <mukta_palshikar@urmc.rochester.edu>
2018-08-17 14:39:43 -0400

    Updated shell scripts for muktaExperiment

--- pyGAsubmit.sh
+++ pyGAsubmit.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 #SBATCH --partition=standard
-#SBATCH -a 1-10
+#SBATCH -a 1-3
 #SBATCH -J GArun
 #SBATCH -o GA_out_%A_%a.txt
 #SBATCH -t 08:00:00
 #SBATCH -n 1
-#SBATCH -c 24
-export OMP_NUM_THREADS=24
+#SBATCH -c 1
 
 module load intelpython/2.7.12
 srun python experiments.py $1 $2 $SLURM_ARRAY_TASK_ID
