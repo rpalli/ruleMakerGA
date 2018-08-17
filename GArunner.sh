e4823a4fb8ce4cce15eb6bb7bf8fa77022ab2a59
mgp13 <mukta_palshikar@urmc.rochester.edu>
2018-08-17 15:24:54 -0400

    Imported ctypes library in experiments.py; corrected loading of c libraries in runExperiment and transformTest

--- GArunner.sh
+++ GArunner.sh
@@ -2,5 +2,8 @@
 chmod -R 755 pyGAsubmit.sh
 for graphfilename in *.gpickle; do
 	chmod -R 755 $graphfilename;
-	sbatch pyGAsubmit.sh $graphfilename 1;
+		for datfilename in *.bin; do
+			chmod -R 755 $datfilename;
+				sbatch pyGAsubmit.sh $graphfilename $datfilename;
+		done		
 done
