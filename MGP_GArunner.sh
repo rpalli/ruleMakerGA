fd51f4045f0fbba38003f87dd900f90d0646f641
mgp13 <mukta_palshikar@urmc.rochester.edu>
2018-08-22 19:03:23 -0400

    Added MGP files for Mukta's experiments - different main class in experiments.py and different shell scripts

---
+++ MGP_GArunner.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+chmod -R 755 pyGAsubmit.sh
+for graphfilename in *.gpickle; do
+	chmod -R 755 $graphfilename;
+		for datfilename in *.bin; do
+			chmod -R 755 $datfilename;
+				sbatch pyGAsubmit.sh $graphfilename $datfilename;
+		done		
+done
---
+++ MGP_pyGAsubmit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH --partition=standard
+#SBATCH -a 1
+#SBATCH -J GArun
+#SBATCH -o GA_out_%A_%a.txt
+#SBATCH -t 08:00:00
+#SBATCH -n 1
+#SBATCH -c 1
+
+module load intelpython/2.7.12
+srun python experiments.py $1 $2 $SLURM_ARRAY_TASK_ID
+echo "ran GA"
